7a0e500d26f1255a296546227f0243d0925dc4e6
Habeeb <ademolawaliyu1@gmail.com>
2024-07-03 13:52:48 +0100

    adding

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// import userRouter from './routes/user.route.js';
-// import authRouter from './routes/auth.route.js';
-// import listingRouter from './routes/listing.route.js';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import cors from 'cors'
@@ -32,9 +32,9 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
-// app.use('/api/user', userRouter);
-// app.use('/api/auth', authRouter);
-// app.use('/api/listing', listingRouter);
+app.use('/', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
